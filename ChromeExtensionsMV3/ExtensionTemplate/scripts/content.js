e8d2aeee1c25df130bb28a0b3d38fb8175f5a030
RaduPantelimon <radu.bucur2@gmail.com>
2023-03-06 14:29:43 +0200

    add template for Empty MV3 Extension that uses Robot JS

---
+++ ChromeExtensionsMV3/ExtensionTemplate/scripts/content.js
@@ -0,0 +1,30 @@
+// Call start
+let processes;
+
+(async() => {
+  //get available processes
+  processes = await UiPathRobot.getProcesses();
+  alert("Number of processes found: " + processes.length);
+
+  //start process - will require GetWorkItemDueDate process available in your UiPath Assistant
+  StartProcess('GetWorkItemDueDate', {in_WIID: "12345"}, processResults => alert("Due Date: " + processResults.out_DueDate));
+
+})();
+
+function StartProcess(processName, arguments, successCallBack)
+{
+	const processToInvoke = processes.find(e => e.name.includes(processName));
+	if(processToInvoke)
+	  processToInvoke.start(arguments).then(
+      successCallBack,
+      err => {
+          //handle error
+          if(err.ClassName && err.ClassName.startsWith("UiPath."))
+            //this is a UiPath Specific Exception
+            alert((err.InnerException && err.InnerException.Message)?err.InnerException.Message:err.Message);
+          else
+            alert(`Start Job failed with the following error message: ${err.toString()}`);
+      });
+	else
+		alert("Process " + processName + " was not found");
+}
